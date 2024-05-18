b67667c99682de1fac442bd4086ee1e1bbc52273
ParasRaba <parasgadhvi12345@gmail.com>
2024-05-18 11:01:39 +0530

    FIX test for checking invalid json
    - The test cases check if the json should return valid or invalid by
    checking the name of the file
    - If the file contains the word `valid` then it should be valid and if
    `invalid` it should be invalid
    - Since `invalid` contains the word `valid`, needed to tweak the if
    condition

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 ## [JSON parser](https://codingchallenges.fyi/challenges/challenge-json-parser)
+
+### NOTE FOR DEVELOPER: To test the package, write a json file in the `testdata` dir, and run the command `make test`
+### If want to test the valid json file, the name must include `valid` init
+- Preferably the format should be `valid.json`, `valid1.json` and so on
+### If want to test the invalid json file, the name must include `invalid` init
+- Preferably the format should be `invalid.json`, `invalid1.json` and so on
--- main_test.go
+++ main_test.go
@@ -7,28 +7,46 @@ import (
 	"testing"
 )
 
+// TestParsing is the sole test to check if parser is validating the json files
+// correctly or not
 func TestParsing(t *testing.T) {
 	testDataDir := os.DirFS("testdata")
-	fs.WalkDir(testDataDir, ".", func(path string, d fs.DirEntry, err error) error {
-		// skip the directory
-		if d.IsDir() {
-			return nil
-		}
-		t.Run(path, func(t *testing.T) {
-			fileContent, err := os.ReadFile("testdata/" + path)
-			if err != nil {
-				t.Errorf("expected to read the file, got: %v", err)
+	err := fs.WalkDir(
+		testDataDir,
+		".",
+		func(path string, d fs.DirEntry, err error) error {
+			// skip the directory
+			if d.IsDir() {
+				return nil
 			}
-			parser := NewParser(fileContent)
-			_, err = parser.Parse()
-			if strings.Contains(path, "invalid") && err == nil {
-				t.Errorf("expected invalid json, got valid")
-			}
-			if strings.Contains(path, "valid") && err != nil {
-				t.Errorf("expected valid json, got: %v", err)
-			}
-		})
+			t.Run(path, func(t *testing.T) {
+				fileContent, err := os.ReadFile("testdata/" + path)
+				if err != nil {
+					t.Errorf(
+						"expected no error in reading the file, got: %v",
+						err,
+					)
+				}
 
-		return nil
-	})
+				parser := NewParser(fileContent)
+				_, err = parser.Parse()
+
+				// invalid json should return some error
+				if strings.Contains(path, "invalid") && err == nil {
+					t.Errorf("expected invalid json, got valid")
+				}
+
+				// valid json should not return any error
+				if strings.Contains(path, "valid") &&
+					!strings.Contains(path, "invalid") &&
+					err != nil {
+					t.Errorf("expected valid json, got: %v", err)
+				}
+			})
+			return nil
+		},
+	)
+	if err != nil {
+		t.Errorf("expected no error in walking dir, got: %v", err)
+	}
 }
